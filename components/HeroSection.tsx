0a4e2a6d51f10eca4df98a2e616325a96046fb56
Natty-ph2 <natnaeldestaer2@gmail.com>
2024-02-01 07:56:08 +0300

    arrow issue fixed

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -44,7 +44,10 @@ const HeroSection = () => {
                   offset={-100}
                   duration={500}
                 >
-                   <HiArrowDown size={35} className="animate-bounce" /> 
+                    <div>
+                    <HiArrowDown size={35} className="animate-bounce" /> 
+                    </div>
+                   
                 </Link>
             </div>
         </section>
