3c0ca37429e1ef26371f32fb0cadc6daeca6e6b7
Natty-ph2 <natnaeldestaer2@gmail.com>
2024-02-01 13:10:15 +0300

    metalayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
+      <meta name="google-site-verification" content="XCagl9ILWtHyarwiwI_dGa1K9DXpc8FozpopeMbYa4A" />
       <head />
       <body className="dark:bg-stone-900">
         <ThemeProvider enableSystem={true} attribute="class">
