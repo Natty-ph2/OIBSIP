783fd37af5be0f020e6d01846d856455a378f7b8
Natty-ph2 <natnaeldestaer2@gmail.com>
2024-03-15 16:48:49 +0300

    todo commit

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react"
+import "./style.css"
+
+export default function App() {
+
+  const [newItem , setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        {id: crypto.randomUUID(), title: newItem, completed: false},
+      ]
+    })
+
+    setNewItem("");
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if(todo.id === id){
+          return{ ...todo, completed}
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id )
+    })
+  }
+ return (
+
+    <>
+         
+            <form onSubmit={handleSubmit} className="new-item-form">
+              <div className="form-row">
+                <label htmlFor="item"><h1>Intern Todolist</h1></label>
+                <input
+                 value={newItem}
+                 onChange={e => setNewItem(e.target.value)}
+                 type="text" 
+                 id="item" />
+              </div>
+              <button className="btn">Add</button>
+            </form>
+            <h1 className="header ">OIBSIP Task Lists</h1>
+            <ui className="list">
+              {todos.length === 0 && "No Todos"}
+              {todos.map(todo => {
+                return (
+                    <li key={todo.id}>
+                    <label >
+                      <input 
+                      type="checkbox"
+                      checked={todo.completed}
+                      onChange={e => toggleTodo(todo.id, e.target.checked) }
+                      />
+                     {todo.title}
+                    </label>
+                    <button 
+                    onClick={() => deleteTodo(todo.id)}
+                    className="btn btn-danger"
+                    >
+                      Delete
+                    </button>
+                    </li>
+                    )
+              })}
+            </ui>
+      </>
+ )
+}
